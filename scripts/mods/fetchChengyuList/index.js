8cb43c5aa8afff7bcf1b183ed92b75319b940035
hxgdzyuyi <hxgdzyuyi@gmail.com>
2017-02-10 20:32:09 +0800

    抽出 fetchChengyuList 处理 chengyuList

--- scripts/mods/fetchChengyuList/index.js
+++ scripts/mods/fetchChengyuList/index.js
@@ -3,11 +3,9 @@ const path = require('path')
 const fs = require('fs')
 const csv = require('csv')
 
-let root = path.join(__dirname, '../../')
-
 module.exports = function() {
   return streamToPromise(
-    fs.createReadStream(path.join(root, './seed/chengyu.csv'))
+    fs.createReadStream(path.join(__dirname, './chengyu.csv'))
       .pipe(csv.parse())
       .pipe(csv.transform((record) => {
         return record[0]
