fc41ee46907152a163735f84a4181a8008336ae3
hxgdzyuyi <hxgdzyuyi@gmail.com>
2017-02-21 19:43:20 +0800

    Fix 找到成语

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,13 +1,31 @@
 'use strict';
 
 import matchChengyuUseTrie from 'MatchChengyu/matchChengyuUseTrie'
+import splitSentence from 'mods/splitSentence'
+
 function main() {
   let contentElement = document.querySelector('#link-report')
 
   if (!contentElement) { return }
 
   let contentText = contentElement.innerText
-  console.log(matchChengyuUseTrie(contentText))
+    , splitedSentences = splitSentence(contentText)
+    , findedChengyu = []
+
+  splitedSentences.forEach((sentence) => {
+    if (sentence.length < 3) { return }
+    let matchedChengyu = matchChengyuUseTrie(sentence)
+
+    if (!matchedChengyu.length) { return }
+
+    matchedChengyu.forEach((chengyu) => {
+      findedChengyu.push(
+        Object.assign(chengyu, { sentence: sentence })
+      )
+    })
+  })
+
+  console.log(findedChengyu)
 }
 
 main()
